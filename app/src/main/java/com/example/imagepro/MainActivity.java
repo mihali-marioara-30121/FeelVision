07dd5f7bd358f7a9c1daff6895bb001c1fb26210
mihali-marioara-30121 <marioaramihali2002@gmail.com>
2024-03-01 22:56:06 +0200

    added TextTospeech instance, working on voice command

--- app/src/main/java/com/example/imagepro/MainActivity.java
+++ app/src/main/java/com/example/imagepro/MainActivity.java
@@ -3,14 +3,29 @@ package com.example.imagepro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.opencv.android.OpenCVLoader;
 
+import java.util.List;
+import java.util.Locale;
+import android.os.Bundle;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech myTTS;
+    private SpeechRecognizer mySpeechRecognizer;
+
     static {
         if(OpenCVLoader.initDebug()){
             Log.d("MainActivity: ","Opencv is loaded");
@@ -27,14 +42,116 @@ public class MainActivity extends AppCompatActivity {
 
         Button camera_button = findViewById(R.id.camera_button);
         camera_button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,CameraActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intent);
+                openCameraActivity();
             }
         });
 
+        initializeTextToSpeech();
+        initializeSpeechRecognizer();
+    }
+
+    private void openCameraActivity() {
+        Intent intent = new Intent(MainActivity.this,CameraActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    private void initializeSpeechRecognizer() {
+        if(SpeechRecognizer.isRecognitionAvailable(this)){
+            mySpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+            mySpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+                @Override
+                public void onReadyForSpeech(Bundle bundle) {
+
+                }
+
+                @Override
+                public void onBeginningOfSpeech() {
+
+                }
+
+                @Override
+                public void onRmsChanged(float v) {
+
+                }
+
+                @Override
+                public void onBufferReceived(byte[] bytes) {
+
+                }
+
+                @Override
+                public void onEndOfSpeech() {
+
+                }
+
+                @Override
+                public void onError(int i) {
+
+                }
+
+                @Override
+                public void onResults(Bundle bundle) {
+                    List<String> results = bundle.getStringArrayList(
+                            SpeechRecognizer.RESULTS_RECOGNITION
+                    );
+                    processResult(results.get(0));
+
+                }
+
+                @Override
+                public void onPartialResults(Bundle bundle) {
+
+                }
+
+                @Override
+                public void onEvent(int i, Bundle bundle) {
+
+                }
+            });
+        }
+    }
+
+    private void processResult(String command) {
+        command = command.toLowerCase();
+        if(command.indexOf("start") != -1){
+            openCameraActivity();
+            speak("CameraActivity is open!");
+        }
+    }
+
+    private void initializeTextToSpeech() {
+        myTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener(){
+
+            @Override
+            public void onInit(int i) {
+                if(myTTS.getEngines().size() == 0){ // no engine are installed
+                    Toast.makeText(MainActivity.this, "There is no TextToSpeech engine on your device!",
+                             Toast.LENGTH_LONG).show();
+                    finish();
+                } else{
+                    myTTS.setLanguage(Locale.ENGLISH);
+                    speak("Hello! I am ready.");
+                }
+            }
+        });
+    }
+
+    private void speak(String message) {
+        if(Build.VERSION.SDK_INT >= 21){
+            myTTS.speak(message, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            myTTS.speak(message, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myTTS.shutdown();
     }
 }
